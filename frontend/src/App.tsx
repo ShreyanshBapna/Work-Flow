4da3fc234af7325a72d81b0e981cc4e2ed659440
shreyansh <shreyansh.jwd@gmail.com>
2025-08-18 17:17:53 +0530

    add / route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Employeeboard from './page/employee'
 function App() {
   return <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Signup/>}/> 
       <Route path="/signup" element={<Signup/>}/> 
       <Route path="/signin" element={<Signin/>}/> 
       <Route path="/dashboard" element={<Dashboard/>}/> 
